4e3e7b9c8cb33d82b3df70054ae604938a5c807c
Stas Vidilin <stanislav.vidilin@gmail.com>
2024-05-02 13:45:40 +0200

    init project structure, add twoSum problem

---
+++ src/test/kotlin/com/stasvidilin/leetcodeproblems/easy/LP00001TwoSumTest.kt
@@ -0,0 +1,30 @@
+package com.stasvidilin.leetcodeproblems.easy
+
+import kotlin.test.Test
+import kotlin.test.assertContentEquals
+
+class LP00001TwoSumTest {
+    @Test
+    fun testTwoSum_case1() {
+        val nums = intArrayOf(2, 7, 11, 15)
+        val target = 9
+        val expectedResult = intArrayOf(0, 1)
+        assertContentEquals(expectedResult, twoSum(nums, target))
+    }
+
+    @Test
+    fun testTwoSum_case2() {
+        val nums = intArrayOf(3, 2, 4)
+        val target = 6
+        val expectedResult = intArrayOf(1, 2)
+        assertContentEquals(expectedResult, twoSum(nums, target))
+    }
+
+    @Test
+    fun testTwoSum_case3() {
+        val nums = intArrayOf(3, 3)
+        val target = 6
+        val expectedResult = intArrayOf(0, 1)
+        assertContentEquals(expectedResult, twoSum(nums, target))
+    }
+}
